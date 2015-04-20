1261a19087349a7a520e7a4af01794d01924cf07
wmenjoy <liujinliang12345@126.com>
2015-04-20 12:57:24 +0800

    Update AndNode.java

--- src/main/java/com/wmenjoy/utils/chain/AndNode.java
+++ src/main/java/com/wmenjoy/utils/chain/AndNode.java
@@ -1,5 +1,20 @@
-package com.wmenjoy.utils.chain;
 
-public class AndNode {
+public class AndNode extends Node {
+
+    //初始化工作链
+    public AndNode(final Node node, final Node next) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void appendNode(final Node next) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected int handle(final Object reqParam, final Object result) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
 }
