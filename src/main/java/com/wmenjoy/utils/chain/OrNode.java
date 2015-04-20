03aa59f3b747a09b87766769b94fc619049b51cf
wmenjoy <liujinliang12345@126.com>
2015-04-20 13:00:13 +0800

    Update OrNode.java

--- src/main/java/com/wmenjoy/utils/chain/OrNode.java
+++ src/main/java/com/wmenjoy/utils/chain/OrNode.java
@@ -5,12 +5,21 @@ package com.wmenjoy.utils.chain;
  * @author jinliang.liu
  *
  */
-public class OrNode extends Node{
-    
+public class OrNode extends Node {
+
+    public OrNode(final Node node, final Node next) {
+        // TODO Auto-generated constructor stub
+    }
+
     @Override
-    protected int handle(Object reqParam, Object result) {
+    protected int handle(final Object reqParam, final Object result) {
         // TODO Auto-generated method stub
         return 0;
+    }
+
+    public void appendNode(final Node next) {
+        // TODO Auto-generated method stub
+
     }
 
 }
