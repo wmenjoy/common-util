058205b01dae66c2e264a4e5bf42f9f42e1d367d
wmenjoy <liujinliang12345@126.com>
2015-04-20 13:01:24 +0800

    Create ParseException.java

---
+++ src/main/java/com/wmenjoy/utils/chain/ParseException.java
@@ -0,0 +1,36 @@
+package com.wmenjoy.utils.chain;
+
+public class ParseException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public ParseException() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public ParseException(final String message, final Throwable cause,
+            final boolean enableSuppression, final boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ParseException(final String message, final Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ParseException(final String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public ParseException(final Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+}
